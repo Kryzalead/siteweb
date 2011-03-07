d2bbee5c850f1c39bf97b174459e19edebb8eb8c
elscorto <elscorto@gmail.com>
2011-03-07 20:55:07 +0100

    ajout include head

--- index.php
+++ index.php
@@ -1,24 +1,11 @@
+<?php
+require('fonctions/config.php');
+?>
 <!DOCTYPE html> 
 <html lang="fr"> 
  
 <head> 
-    <meta charset="utf-8"> 
-    <meta name="author" content="Kryzalead" /> 
-    <title>Kryzalead | Agence web, création de site web</title> 
-    <meta name="DC.title" content="Kryzalead | Agence web, création de site web" /> 
-    <meta name="description" content="Agence web spécialisée dans la conception de sites conformes aux standards W3C et formations HTML, XHTML, CSS, Accessibilité en Charente." /> 
-    <meta name="keywords"  content="creation de sites web, creation de sites internet, webdesign, site web, agence web, webagency, prestations internet, réalisation de sites, charente, 16, la rochelle, 17, standard, standards, w3c, accessibilité, sites accessibles, CSS, CSS3, HTML, HTML5, XHTML, PHP" /> 
-    <meta name="geo.placename" content="Charente, France, La roche" />	
-	<meta name="google-site-verification" content="" /> 
-	<meta name="viewport" content="width=device-width" /> 
- 
-    
-	  <link rel="stylesheet" media="all" href="css/styles.css" /> 
-	  <link rel="stylesheet" media="all" href="css/styles_slide.css" /> 
- 
-    <!--[if IE]><link rel="stylesheet" href="styles-ie.css" /><script src="scripts/html5-ie.js"></script><![endif]--> 
-	<!--[if lte IE 8]><script type="text/javascript" src="scripts/roundies.js"></script><![endif]-->
-
+    <?php include('includes/head.php');?>
 </head>
  
 <body> 
