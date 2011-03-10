b923837cd0ab11ca91d003f3c0ffcf03d98aef03
nici <kryzalead@gmail.com>
2011-03-10 18:19:06 +0100

    pusher mofid form

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,12 @@
-<h1 id="logo"><a href="<?php echo ROOT;?>index.php" title="Retour à l'accueil"><img src="<?php echo ROOT;?>images/logo-kryzalead.png" alt="Kryzalead agence web" width="400" /><span class="accroche">Agence web - Création de sites internet</span></a></h1>
-	<nav role="navigation">
-		<a href="<?php echo ROOT;?>index.php">Accueil</a>
-		<a href="<?php echo ROOT;?>agence.html">L'agence</a>
-		<a href="#">Réalisations</a>
-		<a href="#">Les Packs</a>
-		<a href="#">Devis gratuit</a>
-		<a href="#">Contact</a>
-<!--	<a href="#">Le Blog</a>-->
-	</nav>
+<div id="c_header">
+	<h1 id="logo"><a href="<?php echo ROOT;?>index.php" title="Retour à l'accueil"><img src="<?php echo ROOT;?>images/logo-kryzalead.png" alt="Kryzalead agence web" width="400" /><span class="accroche">Agence web - Création de sites internet</span></a></h1>
+		<nav role="navigation">
+			<a href="<?php echo ROOT;?>index.php">Accueil</a>
+			<a href="<?php echo ROOT;?>agence.html">L'agence</a>
+			<a href="#">Réalisations</a>
+			<a href="#">Les Packs</a>
+			<a href="#">Devis gratuit</a>
+			<a href="#">Contact</a>
+	<!--	<a href="#">Le Blog</a>-->
+		</nav>
+</div>
