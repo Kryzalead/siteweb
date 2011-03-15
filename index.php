8d32763b8dfc135e9d4db8631c252170741bd28f
Nici <kryzalead@gmail.com>
2011-03-15 13:55:54 +0100

    - Correction sémantique HTML <section> <article> - Optimisation CSS - Valid W3C

--- index.php
+++ index.php
@@ -4,94 +4,6 @@
  
 <head> 
     <?php include('includes/head.php');?>
-	
-	<style type="text/css" media="all">
-		/*contact form*/
-			#mask {
-			background-color:#000;
-			display:none;
-			height:100%;
-			left:0;
-			position:fixed;
-			top:0;
-			width:100%;
-			z-index:9000;
-			border: 1px solid red}
-
-			#contact {
-			background-color:#fff;
-			display:none;
-			left:50%;
-			margin-left:-300px;
-			position:absolute;
-			top:90px;
-			width:600px;
-			z-index:9999;
-			border-radius:10px;
-			-moz-border-radius:10px;
-			-webkit-border-radius:10px;
-			padding:20px;
-			border: 1px solid #ccc;}
-
-			#close {
-			background:url(images/contact/close.png) no-repeat right;
-			cursor:pointer;
-			font-family:arial, sans-serif;
-			font-size:20px;
-			font-weight:700;
-			line-height:24px;
-			text-decoration:underline;
-			text-align:right;
-			padding:5px 30px 5px 5px;}
-
-			#contact_header {
-			background:url(images/contact/envelope.png) no-repeat left;
-			font-family:arial, sans-serif;
-			font-size:30px;
-			font-weight:700;
-			line-height:50px;
-			padding:5px 5px 10px 60px;}
-
-			/* form components */
-			input,textarea {
-			border:1px solid silver;
-			background-color:#fff;
-			color:#404040;
-			font-size:10px;
-			font-family:Verdana, Arial, sans-serif;
-			border-radius:5px;
-			-moz-border-radius:5px;
-			-webkit-border-radius:5px;
-			margin:10px 0;
-			padding:10px;}
-
-			input:hover[type=text],input:focus[type=text],textarea:hover,textarea:focus {
-			background-color:#E0E0E0;
-			border:1px solid #000;}
-
-			input[type=text],input[type="email"],textarea {
-			width:300px;}
-
-			#submit {
-			border:none;
-			width:87px;
-			height:41px;
-			border: 1px solid #ccc }
-
-			#submit:hover {
-			cursor:pointer;}
-
-			/* alert messages */
-			.success,.error {
-			display: none}
-
-			.success {
-			background-color:#9F6;
-			border:1px solid #0F0;}
-
-			.error {
-			color: red}
-	</style>
 </head>
  
 <body>
@@ -105,20 +17,22 @@
 		</header>
 
 		<div id="slogan">
-			<h2><a>Votre projet est à l'état de cocon?<br/>Nous sommes là pour l'aider à s'envoler.</a></h2>
+			<h2><a>Votre projet est à l'état de cocon?<span class="retour"></span>
+			Nous sommes là pour l'aider à s'envoler.</a></h2>
 		</div>
 		
 		<div id="content"><!-- Debut content -->
-			<section id="presentation">
-				<div class="colonne" id="colonne_1">
+		
+			<section class="presentation">
+				<article>
 					<h1>L'agence en quelques mots</h1>
 						<p>
 							<span class="color">Kryzalead</span> est une jeune agence web dynamique et structurée. Le sérieux et la motivation de cette équipe qualifiée permettent des réalisations à la fois <span class="souligne">esthétiques, fidèles, fiables et conformes aux stantards du web (W3C)</span>.
 							<a href="agence.html">L'agence</a>
 						</p>
-				</div>
+				</article>
 				
-				<div class="colonne" id="colonne_2">
+				<article>
 					<h1>Projet(s) en cours</h1>
 						<p>
 							<?php
@@ -127,30 +41,30 @@
 								fclose($handle);
 								echo $content;
 							?>			
-					</p>
-				</div>		
+						</p>
+				</article>		
 				
-				<div class="colonne" id="colonne_3">
+				<article>
 					<h1>Votre devis GRATUIT en ligne</h1>
 						<p>
 							Vous avez entre vos mains un projet internet (création de site web, refonte d'un site déjà existant,etc...) à l'état de cocon et souhaitez recevoir rapidement <strong>un devis entièrement gratuit et personnalisé ?</strong>
 							<span class="retour"></span>Remplissez le formulaire en quelques clics afin que nous puissions vous le retourner par mail.
 							<a href="devis/devis.php">Faites votre devis en ligne</a>
 						</p>
-				</div>		
+				</article>		
 				
-				<div class="colonne" id="colonne_4">
+				<article>
 					<h1>Nous contacter</h1>
 						<div id="adresse">
 							<ul>
 								<li><span>Agence Kryzalead</span></li>
 								<li>16220 Ecuras // 17000 La Rochelle</li>
 								<li>Tél: 05&nbsp;45&nbsp;..&nbsp;..&nbsp;..</li>
-								<li>Email:<a href="contact.php">contact@kryzalead.fr</a></li>
+								<li>Email:<a href="contact.php" class="email">contact@kryzalead.fr</a></li>
 								<li>n'hésitez plus à nous contacter.</li>
 							</ul>
-					</div>
-				</div>
+						</div>
+				</article>
 			</section>
 
 			<section class="slideshow">
@@ -163,18 +77,18 @@
 			</section>
 		</div><!-- Fin content -->
 
-		<section id="actu">
+		<section class="actu">
 			<div id="c_actu">
-				<div class="colonne_actu" id="equipe">
+				<article>
 					<h1>L'équipe</h1>
 						<ul>
 							<li><img src="images/footer/avatar-jd.jpg" alt="Intégrateur web et webdesigner" title="Intégrateur web et webdesigner" /><span>Intégrateur web et webdesigner</span>Jean-Damien Demay<a href="cv/webmestre-jd.pdf" class="cv" target="_blank">Mon CV</a></li>
 							<li><img src="images/footer/avatar-jo.jpg" alt="Développeur web" title="Développeur web" /><span>Développeur web</span>Jonathan Arana<a href="cv/webmestre-jo.pdf" class="cv" target="_blank">Mon CV</a></li>
 							<li><img src="images/footer/avatar-kita.jpg" alt="Commerciale" title="Commerciale" /><span>Commerciale</span>Aurélie</li>
 						</ul>
-					</div>
+				</article>
 
-				<div class="colonne_actu" id="blog">
+				<article>
 					<h1>Actu Blog</h1>
 						<ul>
 							<li>01</li>
@@ -183,17 +97,18 @@
 							<li>04</li>
 							<li>05</li>
 						</ul>
-				</div>
+				</article>
 					<!--<aside class="reseaux_sociaux">
 						<a href="#" class="facebook"><img src="images/icon-facebook.png" alt="Retrouvez l'agence web Kryzalead sur Facebook" /><span>Facebook</span></a>
 						<a href="#" class="twitter"><img src="images/icon-twitter.png" alt="Retrouvez l'agence web Kryzalead sur Twitter" /><span>Twitter</span></a>
 					</aside>-->
+			</div>
 		</section>
 		
 	<footer role="contentinfo">
 		<div id="mentions">
 			<p>
-				<span class="color">Kryzalead</span> - 16220 Ecuras - 17000 La Rochelle - Agence web 
+				<span class="color_green">Kryzalead</span> - 16220 Ecuras - 17000 La Rochelle - Agence web 
 				<span style="padding-left:300px">&copy; 2011 - Kryzalead | <a href="mentions-legales.html">Mentions Légales</a> | <a href="plan.html">Plan du site</a> | <a href="w3c.html">Valide W3C</a> | <a href="#wrapper">Haut de page</a></span>
 			</p> 
 		</div>
