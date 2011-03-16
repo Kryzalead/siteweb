181f8e444659192ba5959c344cba05255947f85e
Nici <kryzalead@gmail.com>
2011-03-16 19:35:38 +0100

    - Integration Logo w3c footer

--- index.php
+++ index.php
@@ -116,8 +116,10 @@
 		<div id="mentions">
 			<p>
 				<span class="color_green">Kryzalead</span> - 16220 Ecuras - 17000 La Rochelle - Agence web 
-				<span style="padding-left:300px">&copy; 2011 - Kryzalead | <a href="mentions-legales.html">Mentions Légales</a> | <a href="plan.html">Plan du site</a> | <a href="w3c.html">Valide W3C</a> | <a href="#wrapper">Haut de page</a></span>
-			</p> 
+				<span class="ml">&copy; 2011 - Kryzalead | <a href="mentions-legales.html">Mentions Légales</a> | <a href="plan.html">Plan du site</a> | <a href="w3c.html">Valide W3C</a> | <a href="#wrapper">Haut de page</a></span>
+				<a href="w3c" class="w3c"><img src="images/html5.png" width="70" height="23" alt="HTML5 Powered with CSS3 / Styling, and Semantics" title="HTML5 Powered with CSS3 / Styling, and Semantics" /></a>
+
+			</p>
 		</div>
 	</footer>
 	
