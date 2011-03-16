ce126aab33679492900c1cd76ec55f556325e44e
Nici <kryzalead@gmail.com>
2011-03-16 10:29:09 +0100

    Commentaire sous NAV contact

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <div id="c_header">
-	<h1 id="logo"><a href="<?php echo ROOT;?>index.php" title="Retour à l'accueil"><img src="<?php echo ROOT;?>images/logo-kryzalead.png" alt="Kryzalead agence web" width="450" /><span class="accroche">Agence web - Création de sites internet</span></a></h1>
+	<h1 id="logo"><a href="<?php echo ROOT;?>index.php" title="Retour à l'accueil"><img src="<?php echo ROOT;?>images/logo-kryzalead.png" alt="Kryzalead agence web" title="Retour Accueil - Agence web Kryzalead" width="450" /><span class="accroche">Agence web - Création de sites internet</span></a></h1>
 		<nav role="navigation">
 			<a href="<?php echo ROOT;?>index.php">Accueil</a>
 			<a href="<?php echo ROOT;?>agence.html">L'agence</a>
@@ -9,22 +9,22 @@
 			<a href="contact/index.php" class="popinContact">Contact</a>
 	<!--	<a href="#">Le Blog</a>-->
 		</nav>
-	
-	<!--contact form-->
-	
-<div id="contact">
+			<img src="<?php echo ROOT;?>images/suivez-nous.png" alt="Kryzalead agence web - Suivez-nous - Contact" width="300" class="suivez-nous"/>
+</div>
+
+<div id="contact"><!--contact form-->
 	<div id="close">Fermer</div>
 
 	<h2 id="contact_header">Un projet, un commentaire ?</h2>
-	<p class="success">Merci, votre message a bien été envoyé</p>
-	<p class="error">Une erreur est survenue lors de l'envois du message</p>
-	<form action="traitement.php" method="post" name="contactForm" id="contactForm">
-		<p><label for="nom" id="l_nom">Votre nom : </label><span class="inputError">Invalid username entered!</span><br /><input name="nom" id="nom" type="text" size="30" required /></p>
-		<p><label for="email" id="l_email">Votre email : </label><span class="inputError">Invalid email entered!</span><br /><input name="email" id="email" type="email" size="30" required /></p>
-		<p><label for="message" id="l_message">Votre message : </label><span class="inputError">Invalid message entered!</span><br /><textarea name="message" id="message" rows="5" cols="40" required></textarea></p>
-		<p><input type="submit" id="submit" name="submit" value="Envoyer" /></p>
-	</form>
-</div>
-<!--end contact form-->
+		<p class="success">Merci, votre message a bien été envoyé</p>
+		<p class="error">Une erreur est survenue lors de l'envois du message</p>
+			<form action="traitement.php" method="post" name="contactForm" id="contactForm">
+				<p><label for="nom" id="l_nom">Votre nom : </label><span class="inputError">Invalid username entered!</span><br /><input name="nom" id="nom" type="text" size="30" required /></p>
+				<p><label for="email" id="l_email">Votre email : </label><span class="inputError">Invalid email entered!</span><br /><input name="email" id="email" type="email" size="30" required /></p>
+				<p><label for="message" id="l_message">Votre message : </label><span class="inputError">Invalid message entered!</span><br /><textarea name="message" id="message" rows="5" cols="40" required></textarea></p>
+				<p><input type="submit" id="submit" name="submit" value="Envoyer" /></p>
+			</form>
+</div><!--end contact form-->
+	
 
-</div><div id="mask"></div>
+<div id="mask"></div>
