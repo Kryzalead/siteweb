67add2533775bcecb88dfe678bb18d8816b781b8
Elscorto <elscorto@gmail.com>
2011-03-17 11:53:49 +0100

    include actu ajouté

---
+++ includes/actu.php
@@ -0,0 +1,25 @@
+<div id="c_actu">
+    <article>
+        <h1>L'équipe</h1>
+            <ul>
+                <li><img src="images/footer/avatar-jd.jpg" alt="Intégrateur web et webdesigner" title="Intégrateur web et webdesigner" /><span>Intégrateur web et webdesigner</span>Jean-Damien Demay<a href="cv/webmestre-jd.pdf" class="cv" target="_blank">Mon CV</a></li>
+                <li><img src="images/footer/avatar-jo.jpg" alt="Développeur web" title="Développeur web" /><span>Développeur web</span>Jonathan Arana<a href="cv/webmestre-jo.pdf" class="cv" target="_blank">Mon CV</a></li>
+                <li><img src="images/footer/avatar-kita.jpg" alt="Commerciale" title="Commerciale" /><span>Commerciale</span>Aurélie</li>
+            </ul>
+    </article>
+
+    <article>
+        <h1>Actu Blog</h1>
+            <ul>
+                <li>01</li>
+                <li>02</li>
+                <li>03</li>
+                <li>04</li>
+                <li>05</li>
+            </ul>
+    </article>
+        <!--<aside class="reseaux_sociaux">
+            <a href="#" class="facebook"><img src="images/icon-facebook.png" alt="Retrouvez l'agence web Kryzalead sur Facebook" /><span>Facebook</span></a>
+            <a href="#" class="twitter"><img src="images/icon-twitter.png" alt="Retrouvez l'agence web Kryzalead sur Twitter" /><span>Twitter</span></a>
+        </aside>-->
+</div>
