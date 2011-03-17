bd1509d9df822a512816d47beffa3bc71df992d5
Elscorto <elscorto@gmail.com>
2011-03-17 11:51:32 +0100

    Ajout include footer

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<div id="mentions">
+    <p>
+        <span class="color_green">Kryzalead</span> - 16220 Ecuras - 17000 La Rochelle - Agence web 
+        <span class="ml">&copy; 2011 - Kryzalead | <a href="mentions-legales.html">Mentions Légales</a> | <a href="plan.html">Plan du site</a> | <a href="w3c.html">Valide W3C</a> | <a href="#wrapper">Haut de page</a></span>
+        <a href="w3c" class="w3c"><img src="images/html5.png" width="70" height="23" alt="HTML5 Powered with CSS3 / Styling, and Semantics" title="HTML5 Powered with CSS3 / Styling, and Semantics" /></a>
+
+    </p>
+</div>
