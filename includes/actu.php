2f46f6cdf40db1d7fe94b2be7c4d2bd8194c1b4b
Elscorto <elscorto@gmail.com>
2011-03-17 11:56:12 +0100

    passage au lien absolu sur actu.php

--- includes/actu.php
+++ includes/actu.php
@@ -2,9 +2,9 @@
     <article>
         <h1>L'équipe</h1>
             <ul>
-                <li><img src="images/footer/avatar-jd.jpg" alt="Intégrateur web et webdesigner" title="Intégrateur web et webdesigner" /><span>Intégrateur web et webdesigner</span>Jean-Damien Demay<a href="cv/webmestre-jd.pdf" class="cv" target="_blank">Mon CV</a></li>
-                <li><img src="images/footer/avatar-jo.jpg" alt="Développeur web" title="Développeur web" /><span>Développeur web</span>Jonathan Arana<a href="cv/webmestre-jo.pdf" class="cv" target="_blank">Mon CV</a></li>
-                <li><img src="images/footer/avatar-kita.jpg" alt="Commerciale" title="Commerciale" /><span>Commerciale</span>Aurélie</li>
+                <li><img src="<?php echo ROOT;?>images/footer/avatar-jd.jpg" alt="Intégrateur web et webdesigner" title="Intégrateur web et webdesigner" /><span>Intégrateur web et webdesigner</span>Jean-Damien Demay<a href="<?php echo ROOT;?>cv/webmestre-jd.pdf" class="cv" target="_blank">Mon CV</a></li>
+                <li><img src="<?php echo ROOT;?>images/footer/avatar-jo.jpg" alt="Développeur web" title="Développeur web" /><span>Développeur web</span>Jonathan Arana<a href="<?php echo ROOT;?>cv/webmestre-jo.pdf" class="cv" target="_blank">Mon CV</a></li>
+                <li><img src="<?php echo ROOT;?>images/footer/avatar-kita.jpg" alt="Commerciale" title="Commerciale" /><span>Commerciale</span>Aurélie</li>
             </ul>
     </article>
 
