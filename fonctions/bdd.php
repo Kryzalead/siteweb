081127f73bd765f024ea3a882f25c25491e782fe
elscorto <elscorto@gmail.com>
2011-03-28 19:15:28 +0200

    ajout connexion admin + bdd

---
+++ fonctions/bdd.php
@@ -0,0 +1,4 @@
+<?php
+mysql_connect(SERVER,LOGIN,PASS);
+mysql_selectdb(DB_NAME);
+?>
--- fonctions/config.php
+++ fonctions/config.php
@@ -1,3 +1,8 @@
 <?php
 define('ROOT','/projets/kryzalead/siteweb/');
+
+define('SERVER','localhost');
+define('LOGIN','root');
+define('PASS','root');
+define('DB_NAME','kryzalead');
 ?>
