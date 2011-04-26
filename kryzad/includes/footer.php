26f45033deeeaaf585ff115b1b53d63427646946
Nici <kryzalead@gmail.com>
2011-04-26 12:32:55 +0200

    Signed-off-by: Nici <kryzalead@gmail.com>

---
+++ kryzad/includes/footer.php
@@ -0,0 +1,4 @@
+<div id="footer">
+	  <p>Pour tous renseignements composé le <span class="bleu">05 45 21 19 70</span> ou par mail : <a href="mailto:&#99;%6f%6eta%63%74&#64;k%72%79zal&#101;%61%64%2ef&#114;">contact@kryzalead.fr</a></p>
+	  <p class="copyright">&copy; 2011 - <span class="gras bleu">Kryzalead</span> (Agence web)</p>
+</div>
--- kryzad/indexKryzalead_Admin.php
+++ kryzad/indexKryzalead_Admin.php
@@ -4,46 +4,73 @@
     <head>
         <title>Administration - Agence web Kryzalead</title>
             <?php include('../includes/head.php');?>
-
-                            <link rel="stylesheet" media="all" href="css/styleAdmin.css" /> 
-
+            <link rel="stylesheet" media="all" href="css/style.css" /> 
     </head>
      
     <body onload="slideMenu.build('sm',300,10,10,0)">
         <?php include('../includes/message-ie.html');?>
             
-            <div id="wrapper"> <!-- DEBUT WRAPPER -->
+        <div id="wrapper"> <!-- DEBUT WRAPPER -->
             
                 <header class="menuAdmin" role="banner">
-                    
-                    <div class='gradient' id='v-gradient'>
-                        <h1>Administration du site internet - Kryzalead -</h1>
-                    </div>
-                        <nav role="navigation">
-                            <ul id="sm" class="sm">
-                                <li><img src="images/menu/accueil.png" alt="" />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis nisl id odio molestie adipiscing.</li>
-                                <li><img src="images/menu/pages.png" alt="" />Gestion des Packs <br/> Gérez le contenu de votre site internet rapidement et simplement.</li>
-                                <li><img src="images/menu/portfolio.png" alt="" />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis nisl id odio molestie adipiscing.</li>
-                                <li><img src="images/menu/visiteurs.png" alt="" />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis nisl id odio molestie adipiscing.</li>
-                                <li><img src="images/menu/outils.png" alt="" />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis nisl id odio molestie adipiscing.</li>
-                                <li><img src="images/menu/retour.png" alt="" />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis nisl id odio molestie adipiscing.</li>
-                                <li><img src="images/menu/optionsSup.png" alt="" />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis nisl id odio molestie adipiscing.</li>
-                                <li><img src="images/menu/contact.png" alt="" />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis nisl id odio molestie adipiscing.</li>
-                                <li><img src="images/menu/quitter.png" alt="" />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis nisl id odio molestie adipiscing.</li>
-                            </ul>
-                        </nav>
+				<div class='gradient' id='v-gradient'>
+				    <h1>Bienvenue sur l'administration du site internet <span class="gras bleu">Kryzalead</span></h1>
+				</div>
+				
+				<nav role="navigation">
+					  <ul id="sm" class="sm">
+						    <li><img src="images/menu/accueil.png" alt="" /><span class="titreMenu">Accueil</span>Gérez le contenu de votre site internet rapidement et simplement.</li>
+						    <li><img src="images/menu/pages.png" alt="" /><span class="titreMenu">Gestion des Packs</span>Gérez le contenu de votre site internet rapidement et simplement.</li>
+						    <li><img src="images/menu/portfolio.png" alt="" /><span class="titreMenu">Gestion du Portfolio</span>Gérez le contenu de votre site internet rapidement et simplement.</li>
+						    <li><img src="images/menu/visiteurs.png" alt="" /><span class="titreMenu">Gestion des visiteurs</span>Gérez le contenu de votre site internet rapidement et simplement.</li>
+						    <li><img src="images/menu/outils.png" alt="" /><span class="titreMenu">Outils</span>Gérez le contenu de votre site internet rapidement et simplement.</li>
+						    <li><img src="images/menu/retour.png" alt="" /><span class="titreMenu">Visiualisation</span>Gérez le contenu de votre site internet rapidement et simplement.</li>
+						    <li><img src="images/menu/optionsSup.png" alt="" /><span class="titreMenu">Outils supplémenaires</span>Gérez le contenu de votre site internet rapidement et simplement.</li>
+						    <li><img src="images/menu/contact.png" alt="" /><span class="titreMenu">Gestion des Packs</span>Gérez le contenu de votre site internet rapidement et simplement.</li>
+						    <li><img src="images/menu/quitter.png" alt="" /><span class="titreMenu">Quitter</span>Gérez le contenu de votre site internet rapidement et simplement.</li>
+						    <li></li>
+						    <li></li>
+						    <li></li>
+					  </ul>
+				</nav>
                 </header>
                 
                 <div id="content"><!-- Debut content -->
-                    <section class="repereMenu">
-                        <p><span class="fondVert">Accueil</span><span class="fondGris">Accueil de l'administration</span></p>
-                    </section>
-                        
-                    </section>
-                
+				<section id="repereMenu">
+					  <p>Accueil<span class="decal">Accueil de l'administration</span></p>
+				</section>
+		
+				<section id="evenements">
+				    <h1>Derniers événements</h1>
+						    <div id="cadreEvenements">
+								
+						    </div>
+				</section>
+				
+				
+				<section id="actualites">
+					  <h1>Actualités</h1>
+						    <div id="cadreActualites">
+								
+								
+						    </div>
+				</section>
+			  
+			  
+				<section id="visiteurs">
+					<h1>Statistiques visiteurs</h1>
+						    <div id="cadreVisiteurs">
+						
+						    </div>
+				</section>
                 </div> <!-- FIN CONTENT -->
-            </div> <!-- FIN WRAPPER -->
+		    
+		    <footer role="contentinfo">
+				<?php include('includes/footer.php');?>
+		    </footer>
+	  
+        </div> <!-- FIN WRAPPER -->
             
             <script type="text/javascript" src="scripts/slidemenu.js"></script>
-    </body>
+	  </body>
 </html>
